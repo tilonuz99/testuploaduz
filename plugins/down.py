f3b907fa50afd077ecfaf627be988bdc51dca8e5
tilonuz <59096186+tilonuz99@users.noreply.github.com>
2020-10-31 19:08:34 +0500

    Update down.py

--- plugins/down.py
+++ plugins/down.py
@@ -39,41 +39,18 @@ async def down_load_media(client, message):
     mes = await message.reply_text("Tekshirilmoqda...")
     custom_file_name = os.path.basename(url)
     try:
-    	downloader = SmartDL(url, 'downloads', progress_bar=False)
+    	downloader = SmartDL(url, 'downloads', progress_bar=True)
     	downloader.start(blocking=False)
     except Exception as e:
     	await mes.edit("Xatolik: Bunday manzil mavjud emas!")
     	return
     c_time = time.time()
-    display_message = ""
-    while not downloader.isFinished():
-        total_length = downloader.filesize if downloader.filesize else None
-        downloaded = downloader.get_dl_size()
-        now = time.time()
-        diff = now - c_time
-        percentage= downloader.get_progress() * 100
-        speed = downloader.get_speed()
-        elapsed_time = round(diff) * 1000
-        progress_str = "[{0}{1}]\nProgress: {2}%".format(''.join(["█" for i in range(math.floor(percentage / 5))]),''.join(["░" for i in range(20 - math.floor(percentage / 5))]),round(percentage, 2))
-        estimated_total_time = downloader.get_eta(human=True)
-        try:
-        	current_message = f"Yuklab olinmoqda...\n"
-        	current_message += f"{progress_str}\n"
-        	current_message += f"{humanbytes(downloaded)} of {humanbytes(total_length)}\n"
-        	current_message += f"Vaqt: {estimated_total_time}"
-        	if round(diff % 10.00) == 0 and current_message != display_message:
-        		await mes.edit(current_message)
-        		display_message = current_message
-        		await asyncio.sleep(1)
-        except:
-        	pass
     if downloader.isSuccessful():
         await mes.edit("Yuborilmoqda...")
         download_file_path = downloader.get_dest()
-        duration = int(get_sec(getvid(download_file_path)[0]))
         width = int(getvid(download_file_path)[1])
         height = int(getvid(download_file_path)[2])
-        send = await client.send_video(mes.chat.id,video=download_file_path,thumb=thumbs,duration=duration,width=width,height=height,progress=progress_for_pyrogram,progress_args=("Yuborilmoqda...",mes,start_time))
+        send = await client.send_video(mes.chat.id,video=download_file_path,thumb=thumbs,width=width,height=height,progress=progress_for_pyrogram,progress_args=("Yuborilmoqda...",mes,start_time))
         if send:
         	await mes.edit("Yuborildi")
         	os.remove(download_file_path)
